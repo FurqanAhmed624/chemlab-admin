75594d68de6ee4413c280e05575a991d1ff988d7
furqanahmed624 <furqan.ahmed@kalpayfinancials.com>
2025-09-02 16:25:04 +0500

    removed side-bar Footer

--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -5,18 +5,8 @@ import {
   CollapsibleTrigger
 } from '@/components/ui/collapsible';
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger
-} from '@/components/ui/dropdown-menu';
-import {
   Sidebar,
   SidebarContent,
-  SidebarFooter,
   SidebarGroup,
   SidebarGroupLabel,
   SidebarHeader,
@@ -28,22 +18,14 @@ import {
   SidebarMenuSubItem,
   SidebarRail
 } from '@/components/ui/sidebar';
-import { UserAvatarProfile } from '@/components/user-avatar-profile';
-import { navItems } from '@/constants/data';
+// import { UserAvatarProfile } from '@/components/user-avatar-profile';
+// import { navItems } from '@/constants/data';
 import { useMediaQuery } from '@/hooks/use-media-query';
 // REMOVED: import { useUser } from '@clerk/nextjs';
-import {
-  IconBell,
-  IconChevronRight,
-  IconChevronsDown,
-  IconCreditCard,
-  IconLogout,
-  IconPhotoUp,
-  IconUserCircle
-} from '@tabler/icons-react';
+import { IconChevronRight, IconPhotoUp } from '@tabler/icons-react';
 // REMOVED: import { SignOutButton } from '@clerk/nextjs';
 import Link from 'next/link';
-import { usePathname, useRouter } from 'next/navigation';
+import { usePathname } from 'next/navigation';
 import * as React from 'react';
 import { Icons } from '../icons';
 import { OrgSwitcher } from '../org-switcher';
@@ -67,19 +49,18 @@ const tenants = [
   { id: '3', name: 'Gamma Ltd' }
 ];
 
-const mockUser = {
-  firstName: 'Guest',
-  lastName: 'User',
-  imageUrl: `https://avatar.vercel.sh/guest-user.png`, // Using a dynamic placeholder avatar
-  primaryEmailAddress: {
-    emailAddress: 'guest@example.com'
-  }
-};
+// const mockUser = {
+//   firstName: 'Guest',
+//   lastName: 'User',
+//   imageUrl: `https://avatar.vercel.sh/guest-user.png`, // Using a dynamic placeholder avatar
+//   primaryEmailAddress: {
+//     emailAddress: 'guest@example.com'
+//   }
+// };
 
 export default function AppSidebar() {
   const pathname = usePathname();
   const { isOpen } = useMediaQuery();
-  const router = useRouter();
   // REMOVED: const { user } = useUser();
 
   const [navItems, setNavItems] = useState(staticNavItems);
@@ -194,71 +175,6 @@ export default function AppSidebar() {
         </SidebarGroup>
       </SidebarContent>
 
-      <SidebarFooter>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <SidebarMenuButton
-                  size='lg'
-                  className='data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground'
-                >
-                  {/*{mockUser && (*/}
-                  {/*  <UserAvatarProfile*/}
-                  {/*    className='h-8 w-8 rounded-lg'*/}
-                  {/*    showInfo*/}
-                  {/*    // Pass the mock user object to the profile component*/}
-                  {/*    user={mockUser}*/}
-                  {/*  />*/}
-                  {/*)}*/}
-                  <IconChevronsDown className='ml-auto size-4' />
-                </SidebarMenuButton>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent
-                className='w-(--radix-dropdown-menu-trigger-width) min-w-56 rounded-lg'
-                side='bottom'
-                align='end'
-                sideOffset={4}
-              >
-                <DropdownMenuLabel className='p-0 font-normal'>
-                  {/*<div className='px-1 py-1.5'>*/}
-                  {/*  {mockUser && (*/}
-                  {/*    <UserAvatarProfile*/}
-                  {/*      className='h-8 w-8 rounded-lg'*/}
-                  {/*      showInfo*/}
-                  {/*      user={mockUser}*/}
-                  {/*    />*/}
-                  {/*  )}*/}
-                  {/*</div>*/}
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuGroup>
-                  <DropdownMenuItem
-                    onClick={() => router.push('/dashboard/profile')}
-                  >
-                    <IconUserCircle className='mr-2 h-4 w-4' />
-                    Profile
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <IconCreditCard className='mr-2 h-4 w-4' />
-                    Billing
-                  </DropdownMenuItem>
-                  <DropdownMenuItem>
-                    <IconBell className='mr-2 h-4 w-4' />
-                    Notifications
-                  </DropdownMenuItem>
-                </DropdownMenuGroup>
-                <DropdownMenuSeparator />
-                {/* 3. Replace the SignOutButton with a simple text element */}
-                <DropdownMenuItem onClick={() => alert('Sign out clicked!')}>
-                  <IconLogout className='mr-2 h-4 w-4' />
-                  <span>Sign Out</span>
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarFooter>
       <SidebarRail />
     </Sidebar>
   );
